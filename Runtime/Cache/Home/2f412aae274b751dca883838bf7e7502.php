a270b2d291018200194d0bbc5b161ae929194985
luxudong <lxd22678@163.com>
2014-09-06 14:38:31 +0800

    some file not upload

---
+++ Runtime/Cache/Home/2f412aae274b751dca883838bf7e7502.php
@@ -0,0 +1,45 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+    
+        <title>用于个人测试的平台</title>
+    
+
+    <link rel="stylesheet" type="text/css" href="/Public/css/base.css"/>
+    <link rel="shortcut icon" type="image/ico" href="/Public/images/icon/favicon.ico">
+    <script type="text/javascript" src="/Public/js/jquery-1.11.1.js"></script>
+    <script type="text/javascript" src="/Public/js/bootstrap.js"></script>
+    <script type="text/javascript" src="/Public/js/jquery.cookie.js"></script>
+    <script type="text/javascript" src="/Public/js/jquery.PrintArea.js"></script>
+
+    
+
+    
+
+</head>
+
+<body>
+
+    <div class="container">
+        
+    <div class="row">
+        <div class="span12 center-block">
+            欢迎光临
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-4">
+            <form class="form-horizontal">
+                <div class="form-group">
+                    <label for="inputname">出版社名</label>
+                    <input type="text" class="form-control" id="inputname" placeholder="请输入姓名">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    </div>
+
+</body>
+
+</html>
